9dd4212987bde76c53c2049a9932714fd7da4156
ElenaPetrus <leliksmp@ukr.net>
2021-06-11 21:10:46 +0300

    05 modul added

---
+++ js/05.js
@@ -0,0 +1,42 @@
+/*=============================1=====================*/
+// const parent = {
+//   name: 'Stacey',
+//   surname: 'Moore',
+//   age: 54,
+//   heritage: 'Irish'
+// };
+
+// const child = Object.create(parent);
+
+// child.name = 'Jason';
+// child.age = 27;
+
+/*=============================2=====================*/
+// const ancestor = {
+//   name: 'Paul',
+//   age: 83,
+//   surname: 'Dawson',
+//   heritage: 'Irish'
+// };
+// // Пиши код ниже этой строки
+
+// const parent = Object.create(ancestor);
+// parent.name = 'Stacey';
+// parent.surname = 'Moore';
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = 'Jason';
+// child.age = 27;
+
+/*=============================3=====================*/
+// function Car {
+//   constructor(brand, model, price) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// };
+
+// const myCar = new Car('Audi', 'Q3', 36000);
+// console.log(myCar);
